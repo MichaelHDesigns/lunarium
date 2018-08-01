a988a286bc12b3663a2eea364a529beeb96988b5
RealHoneyBadger <38737396+RealHoneyBadger@users.noreply.github.com>
2018-08-01 09:34:14 +0200

    Update kernel.cpp
    ModifierUpgradeBlock removal

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -83,8 +83,8 @@ static bool SelectBlockFromCandidates(
     uint64_t nStakeModifierPrev,
     const CBlockIndex** pindexSelected)
 {
-    bool fModifierV2 = false;
-    bool fFirstRun = true;
+ //   bool fModifierV2 = false;
+ //   bool fFirstRun = true;
     bool fSelected = false;
     uint256 hashBest = 0;
     *pindexSelected = (const CBlockIndex*)0;
@@ -97,20 +97,20 @@ static bool SelectBlockFromCandidates(
             break;
 
         //if the lowest block height (vSortedByTimestamp[0]) is >= switch height, use new modifier calc
-        if (fFirstRun){
-            fModifierV2 = pindex->nHeight >= Params().ModifierUpgradeBlock();
-            fFirstRun = false;
-        }
+   //     if (fFirstRun){
+   //         fModifierV2 = pindex->nHeight >= Params().ModifierUpgradeBlock();
+   //         fFirstRun = false;
+   //     }
 
         if (mapSelectedBlocks.count(pindex->GetBlockHash()) > 0)
             continue;
 
         // compute the selection hash by hashing an input that is unique to that block
         uint256 hashProof;
-        if(fModifierV2)
-            hashProof = pindex->GetBlockHash();
-        else
-            hashProof = pindex->IsProofOfStake() ? 0 : pindex->GetBlockHash();
+   //     if(fModifierV2)
+        hashProof = pindex->GetBlockHash();
+  //      else
+  //          hashProof = pindex->IsProofOfStake() ? 0 : pindex->GetBlockHash();
 
         CDataStream ss(SER_GETHASH, 0);
         ss << hashProof << nStakeModifierPrev;
