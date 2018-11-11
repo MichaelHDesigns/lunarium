82cf4337a1f5b3a4f73ed57eee21ca5ff30d0f68
Etienne Savard <esavard@symbiosoft.net>
2018-11-11 09:10:50 -0500

    Fix clients from not connecting to peers by not using legacy dirty versions inherited from PIVX or Phore. Don't use: 1.1.0, 1.3.0, nor 1.3.1 to avoid side effects.

--- src/main.cpp
+++ src/main.cpp
@@ -5755,14 +5755,14 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
             vRecv >> LIMITED_STRING(pfrom->strSubVer, 256);
             pfrom->cleanSubVer = SanitizeString(pfrom->strSubVer);
         }
-        // broken releases with wrong blockchain data
-        if (pfrom->cleanSubVer == "/Lunarium:1.1.0/" ||
+        // DIRTY RELEASES: broken releases with wrong blockchain data
+        /*if (pfrom->cleanSubVer == "/Lunarium:1.1.0/" ||
             pfrom->cleanSubVer == "/Lunarium:1.3.0/" ||
             pfrom->cleanSubVer == "/Lunarium:1.3.1/") {
             LOCK(cs_main);
             Misbehaving(pfrom->GetId(), 100); // instantly ban them because they have bad block data
             return false;
-        }
+        }*/
         if (!vRecv.empty())
             vRecv >> pfrom->nStartingHeight;
         if (!vRecv.empty())
