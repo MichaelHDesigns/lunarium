be1002c235c361a94ae8446eb751f4699a5a2ef4
Jack Coble <jackkdev@protonmail.com>
2019-03-28 18:16:26 +0000

    Switch payment cycle once at blockheight

--- src/masternode-budget.cpp
+++ src/masternode-budget.cpp
@@ -40,7 +40,11 @@ int GetBudgetPaymentCycleBlocks()
     if (Params().NetworkID() == CBaseChainParams::MAIN) return 43200;
     //for testing purposes
 
-    return 720; //twice per day
+    if (chainActive.Height() >= SOFT_FORK_BLOCK) {
+        return 720; // twice per day
+    } else {
+        return 144; //ten times per day
+    }
 }
 
 bool IsBudgetCollateralValid(uint256 nTxCollateralHash, uint256 nExpectedHash, std::string& strError, int64_t& nTime, int& nConf, bool fBudgetFinalization)
@@ -176,7 +180,11 @@ void CBudgetManager::SubmitFinalBudget()
         // NOTE: 9 blocks for testnet is way to short to have any masternode submit an automatic vote on the finalized(!) budget,
         //       because those votes are only submitted/relayed once every 56 blocks in CFinalizedBudget::AutoCheck()
 
-        finalizationWindow = 214; // 180 + 4 finalization confirmations + 5 minutes (30 blocks) buffer for propagation
+        if (chainActive.Height() >= SOFT_FORK_BLOCK) {
+            finalizationWindow = 214; // 180 + 4 finalization confirmations + 5 minutes (30 blocks) buffer for propagation
+        } else {
+            finalizationWindow = 64; // 56 + 4 finalization confirmations + 4 minutes buffer for propagation
+        }
     }
 
     int nFinalizationStart = nBlockStart - finalizationWindow;
